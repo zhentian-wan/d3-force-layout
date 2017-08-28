07a2f87ce2cc1da898447d902774fd739e34b92c
zwan <zhentian.wan@ssh.com>
2017-08-28 15:49:14 +0300

    force layout intro

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ForceLayoutIntro from './components/ForceLayoutIntro';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        
+        <ForceLayoutIntro width={960} height={600} ></ForceLayoutIntro>
+
+      </div>
+    );
+  }
+}
+
+export default App;
