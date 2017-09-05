f6e2085333e50a7e8febd80e497d13edb9b2f337
Zhentian Wan <answer881215@gmail.com>
2017-09-05 22:49:24 +0300

    read data from csv

--- src/components/Throns.js
+++ src/components/Throns.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import * as d3  from 'd3';
+const url = require('../battles.csv');
 
 export default class Throns extends Component {
 
     componentDidMount() {
 
-        d3.csv('../battles.csv', function (data) {
-            console.log("error", data);
+        d3.csv(url, function (links) {
+
+            // create empty nodes array
+            var nodes = {};
+
+            // compute nodes from links data
+            links.forEach(function(link) {
+                link.source = nodes[link.source] ||
+                    (nodes[link.source] = {name: link.source});
+                link.target = nodes[link.target] ||
+                    (nodes[link.target] = {name: link.target});
+                link.value = +link.value;
+            });
+
+
+            // log to console for debugging
+            console.log(JSON.stringify(links, null, 4));
+            console.log('nodes:');
+            console.log(JSON.stringify(nodes, null, 4));
         });
 
     }
