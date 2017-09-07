2bb1f6ddbacac34886eaa489a48e5f6acb83717c
Zhentian Wan <answer881215@gmail.com>
2017-09-07 17:53:40 +0300

    done

--- src/App.js
+++ src/App.js
@@ -11,18 +11,14 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
                 <section>
                     <h2>Game of throns</h2>
                     <Throns width={960} height={600}/>
                 </section>
-                <section>
+                <!-- <section>
                     <h2>Simple Example</h2>
                     <SimpleExample width={960} height={600}></SimpleExample>
-                </section>
+                </section> -->
                 <section>
                     <h2>Force Layout intro</h2>
                     <ForceLayoutIntro width={960} height={600}></ForceLayoutIntro>
